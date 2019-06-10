99db902d85956177d8390af4e6d08c3b87d2926c
Руслан Муфтиев <muftik@yandex-team.ru>
2019-06-10 22:24:21 +0300

    React-redux starting app

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux'
+import reducer from '../reducer'
+import logger from '../middlewares/logger'
+import thunk from 'redux-thunk'
+
+const enhancer = applyMiddleware(thunk, logger)
+
+const store = createStore(reducer, enhancer)
+
+//dev only, no need in prod!
+window.store = store
+
+export default store
