840939f273a620dd7ca4c586c54a859de6289b34
Alexander Vlasov <stuartrevol@gmail.com>
2019-06-12 17:31:01 +0300

    firebase delpying

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import App from './components/app'
 import store from './store'
-import history from './history'
+import history from './helpers/history'
+import * as serviceWorker from './helpers/serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,3 +15,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
+serviceWorker.register()
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import counterReducer from './counter'
-import history from '../history'
+import history from '../helpers/history'
 
 export default combineReducers({
   counter: counterReducer,
